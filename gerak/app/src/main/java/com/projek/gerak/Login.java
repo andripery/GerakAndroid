fe3b8af9911a78b6c4dc9552aceba3623864210a
andripery <andripery@gmail.com>
2020-01-16 12:18:14 +0700

    16 Januari 2020 Autentikasi Login *Belum rapi

--- gerak/app/src/main/java/com/projek/gerak/Login.java
+++ gerak/app/src/main/java/com/projek/gerak/Login.java
@@ -2,20 +2,143 @@ package com.projek.gerak;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Login extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class Login extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = "LoginActivity";
+
+    private DatabaseReference mDatabase;
+    private FirebaseAuth mAuth;
+    private EditText edtEmail;
+    private EditText edtPass;
+    private Button btnMasuk;
+    private Button btnDaftar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+        //variabel tadi untuk memanggil fungsi
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mAuth = FirebaseAuth.getInstance();
+
+        edtEmail = (EditText) findViewById(R.id.tbEmail);
+        edtPass = (EditText) findViewById(R.id.tbPassword);
+        btnMasuk = (Button) findViewById(R.id.btnLogin);
+
+        //nambahin method onClick, biar tombolnya bisa diklik
+        btnMasuk.setOnClickListener(this);
+
     }
 
-    public void home(View view){
+    //fungsi signin untuk mengkonfirmasi data pengguna yang sudah mendaftar sebelumnya
+    private void signIn() {
+        Log.d(TAG, "signIn");
+        if (!validateForm()) {
+            return;
+        }
+
+        //showProgressDialog();
+        String email = edtEmail.getText().toString();
+        String password = edtPass.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signIn:onComplete:" + task.isSuccessful());
+                        //hideProgressDialog();
+
+                        if (task.isSuccessful()) {
+                            onAuthSuccess(task.getResult().getUser());
+                        } else {
+                            Toast.makeText(Login.this, "Sign In Failed",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    //fungsi dipanggil ketika proses Authentikasi berhasil
+    private void onAuthSuccess(FirebaseUser user) {
+        String username = usernameFromEmail(user.getEmail());
+
+        // membuat User baru
+        writeNewUser(user.getUid(), username, user.getEmail());
+
+        // Go to MainActivity
+        startActivity(new Intent(Login.this, Home.class));
+        finish();
+    }
+
+    /*
+       ini fungsi buat bikin username dari email
+           contoh email: abcdefg@mail.com
+           maka username nya: abcdefg
+    */
+    private String usernameFromEmail(String email) {
+        if (email.contains("@")) {
+            return email.split("@")[0];
+        } else {
+            return email;
+        }
+    }
+
+    //fungsi untuk memvalidasi EditText email dan password agar tak kosong dan sesuai format
+    private boolean validateForm() {
+        boolean result = true;
+        if (TextUtils.isEmpty(edtEmail.getText().toString())) {
+            edtEmail.setError("Required");
+            result = false;
+        } else {
+            edtEmail.setError(null);
+        }
+
+        if (TextUtils.isEmpty(edtPass.getText().toString())) {
+            edtPass.setError("Required");
+            result = false;
+        } else {
+            edtPass.setError(null);
+        }
+
+        return result;
+    }
+
+    // menulis ke Database
+    private void writeNewUser(String userId, String name, String email) {
+        User user = new User(name, email);
+
+        mDatabase.child("admins").child(userId).setValue(user);
+    }
+
+    public void home(View view) {
         Intent intent = new Intent(getApplicationContext(), Home.class);
         startActivity(intent);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int i = v.getId();
+        if (i == R.id.btnLogin) {
+            signIn();
+        }
     }
 }
